e99feca24cf1ff507f5cc21f238db1e85cead4c3
publiminal <publiminal@gmail.com>
2022-05-19 02:32:22 -0400

    init

--- api/server.js
+++ api/server.js
@@ -8,3 +8,23 @@ server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
 module.exports = server;
+
+const db = require('../data/db-config')
+/**
+  [GET] /api/schemes
+
+  response:
+  [
+    { scheme_id: 1, scheme_name: "World Domination", number_of_steps: 3 },
+    { scheme_id: 2, scheme_name: "Get Rich Quick", number_of_steps: },
+  ]
+ */
+
+  async function query(){
+       const query = await db('schemes')
+       console.log(query)
+  }
+  query()
+
+
+
--- queries.sql
+++ queries.sql
@@ -7,3 +7,26 @@
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+-- GET /api/schemes
+    -- response:
+    -- [ { scheme_id: 1, scheme_name: "World Domination", number_of_steps: 3 }, { scheme_id: 2, scheme_name: "Get Rich Quick", number_of_steps: } ...]
+
+SELECT * FROM schemes LEFT JOIN steps ON schemes.scheme_id = steps.scheme_id ;  
+
+SELECT  schemes.*,  
+        count(*) as number_of_steps 
+    FROM schemes 
+        LEFT JOIN steps 
+            ON schemes.scheme_id = steps.scheme_id  
+        GROUP BY schemes.scheme_id ;
+
+
+      SELECT
+          sc.*,
+          count(st.step_id) as number_of_steps
+      FROM schemes as sc
+      LEFT JOIN steps as st
+          ON sc.scheme_id = st.scheme_id
+      GROUP BY sc.scheme_id
+      ORDER BY sc.scheme_id ASC;
